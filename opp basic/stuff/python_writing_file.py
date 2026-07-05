c425ea86bf0b637fdf952c4d6b5f024a461af491
Samuel <nushuhu51@gmail.com>
2026-07-05 19:08:35 +0300

    write csv file

--- opp basic/stuff/python_writing_file.py
+++ opp basic/stuff/python_writing_file.py
@@ -2,15 +2,19 @@
 
 # Text data
 import json
-employees = {"name": "Spongebob",
-             "age": 30 ,
-             "job": "Cook"
-             }
+import csv
 
-file_path = "C:/Users/Samuel/OneDrive/Desktop/test.json"
+employees = [[ "name", "Age", "Job"],
+             [ "Sponge", 30 , "Cook"],
+             ["Patrick", 37, "Unemployeed"],
+             ["Sandy", 27, "Scintist"]]
+
+file_path = "C:/Users/Samuel/OneDrive/Desktop/test.csv"
 try:
     with open(file=file_path, mode="w") as file:
-        json.dump(employees, file, indent=4)
-        print(f"json file {file_path} was created")
+        writer = csv.writer(file)
+        for row in employees:
+            writer.writerow(row)
+        print(f"CSV file {file_path} was created")
 except FileExistsError:
     print("That file already exist!")
