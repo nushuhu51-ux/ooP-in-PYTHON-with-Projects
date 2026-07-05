94ebf4e231754f8e977570b88f88782e71d94493
Samuel <nushuhu51@gmail.com>
2026-07-05 16:01:20 +0300

    File detection

---
+++ opp basic/python_file_detection.py
@@ -0,0 +1,16 @@
+# Python file detection
+
+import os
+file_path = "C:/Users/Samuel/OneDrive/Desktop/test"
+
+if os.path.exists(file_path):
+    print(f"The location {file_path} exists")
+    
+    if os.path.isfile(file_path):
+        print("That is a file")
+    elif os.path.isdir(file_path):
+        print("That is a directory")
+        
+        
+else: 
+    print("That location doesn't exists")
