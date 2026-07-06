93c211d2741520c296100a6d39ed094130e28f62
Samuel <nushuhu51@gmail.com>
2026-07-06 16:22:54 +0300

    PyQt QLabels

--- opp basic/PyGt5_QLabels.py
+++ opp basic/PyGt5_QLabels.py
@@ -5,26 +5,34 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        #self.setWindowTitle("My cool first GUI")
+
         self.setGeometry(700, 300, 500, 500)
-        #self.setWindowIcon(QIcon("profile_pic.jpg"))
+
         label = QLabel("Hello", self)
         label.setFont(QFont("Arial", 30))
         label.setGeometry(0, 0, 500, 100)
-        label.setStyleSheet("color: #292929,"
-                            "background-color: #6fdcf7,"
-                            "font-weight: bold,"
-                            "fint-style: italic,"
-                            "text-decoration: underline,")
-        label.setAlignment(Qt.AlignBottom)
-    def main():
-        app = QApplication(sys.argv)
-        window = MainWindow()
-        window.show()
-        sys.exit(app.exec_())
-        
-        if __name__ == "__main__":
-            main()
+
+        label.setStyleSheet("""
+            color: #292929;
+            background-color: #6fdcf7;
+            font-weight: bold;
+            font-style: italic;
+            text-decoration: underline;
+        """)
+
+        label.setAlignment(Qt.AlignCenter)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
