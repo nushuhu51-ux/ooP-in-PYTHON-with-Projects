4fcc502203e30d82f009a1698b05f8931b6fc4f8
Samuel <nushuhu51@gmail.com>
2026-07-06 16:16:56 +0300

    PyQt images

--- opp basic/PyQt5_images.py
+++ opp basic/PyQt5_images.py
@@ -3,34 +3,34 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QPixmap
 
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
-        supper().__init__()
-        #self.setWindowTitle("My cool first GUI")
+        super().__init__()
+
+        self.setWindowTitle("Image Viewer")
         self.setGeometry(700, 300, 500, 500)
-        
+
         label = QLabel(self)
-        label.setGeometry(0, 0, 500, 500)
-        
-        pixmap = QPixmap("profile.jpg")
-        label.setPixmap(pixmap)
-        
-        label.setScaleContents(True)
-        
-        label.setGeometry((self.width() - label.width()) // 2,
-                          (self.height() - label.height()) // 2,
-                          label.width(), 
-                          label.height())
+        label.setGeometry(0, 0, self.width(), self.height())
 
+        pixmap = QPixmap(
+            "C:/Users/Samuel/OneDrive/Desktop/OPP PYTHON/opp basic/photo_2026-07-06_16-11-36.jpg"
+        )
 
+        if pixmap.isNull():
+            print("Image could not be loaded!")
+        else:
+            label.setPixmap(pixmap)
+
+        label.setScaledContents(True)
 
 
 def main():
-        app = QApplication(sys.argv)
-        window = MainWindow()
-        window.show()
-        sys.exit(app.exec_())
-        
-        if __name__ == "__main__":
-            main()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
