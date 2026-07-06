0f7de85624841a054d2662238b9977ffc9511f0f
Samuel <nushuhu51@gmail.com>
2026-07-06 16:08:38 +0300

    push button widget

--- opp basic/PyQt5_Layouts.py
+++ opp basic/PyQt5_Layouts.py
@@ -1,51 +1,53 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel,
-                             QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
-
-
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QLabel,
+    QWidget,
+    QGridLayout
+)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-    
-        self.setGeometry(700, 300, 500, 500)
-    def initUI(self):
-        centeral_widget = QWidget()
-        self.setCentralWidget(centeral_widget)
-        
-        label1 = QLabel("#1", self)
-        label2 = QLabel("#2", self)
-        label3 = QLabel("#3", self)
-        label4 = QLabel("#4", self)
-        label5 = QLabel("#5", self)
-        
-        label1.setStyleSheet("backgroung-color: red,")
-        label2.setStyleSheet("backgroung-color: yellow,") 
-        label3.setStyleSheet("backgroung-color: green,") 
-        label4.setStyleSheet("backgroung-color: blue,") 
-        label5.setStyleSheet("backgroung-color: purple,") 
-        
-        vbox = QVBoxLayout()
-        
-        vbox.addWidget(label1, 0, 0)
-        vbox.addWidget(label2, 0, 1)
-        vbox.addWidget(label3, 1, 0)
-        vbox.addWidget(label4, 1, 1)
-        vbox.addWidget(label5, 1, 2)
-        
-        centeral_widget.setLayout(vbox)
-        
-        label = QLabel(self)
-        label.setGeometry(0, 0, 500, 500)
 
+        self.setGeometry(700, 300, 500, 500)
+        self.initUI()
+
+    def initUI(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        label1 = QLabel("#1")
+        label2 = QLabel("#2")
+        label3 = QLabel("#3")
+        label4 = QLabel("#4")
+        label5 = QLabel("#5")
+
+        label1.setStyleSheet("background-color: red;")
+        label2.setStyleSheet("background-color: yellow;")
+        label3.setStyleSheet("background-color: green;")
+        label4.setStyleSheet("background-color: blue; color: white;")
+        label5.setStyleSheet("background-color: purple; color: white;")
+
+        grid = QGridLayout()
+
+        grid.addWidget(label1, 0, 0)
+        grid.addWidget(label2, 0, 1)
+        grid.addWidget(label3, 1, 0)
+        grid.addWidget(label4, 1, 1)
+        grid.addWidget(label5, 1, 2)
+
+        central_widget.setLayout(grid)
 
 
 def main():
-        app = QApplication(sys.argv)
-        window = MainWindow()
-        window.show()
-        sys.exit(app.exec_())
-        
-        if __name__ == "__main__":
-            main()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
