ca1c7e82a993126572eb424106ee621d4450a6cf
Samuel <nushuhu51@gmail.com>
2026-07-06 16:03:51 +0300

    push button widget

--- opp basic/push_button_widget.py
+++ opp basic/push_button_widget.py
@@ -1,34 +1,37 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
-from PyQt5.QtGui import QPixmap
-         
-         
-         
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
         self.setGeometry(700, 300, 500, 500)
+        self.setWindowTitle("Button Example")
+
         self.initUI()
-        
+
     def initUI(self):
-        self.button = QPushButton("Click me!", self)
+        self.button = QPushButton("Click Me!", self)
         self.button.setGeometry(150, 200, 200, 100)
-        self.button.setStyleSheet("font-size: 30px")
+
+        self.button.setStyleSheet("""
+            font-size: 30px;
+        """)
+
         self.button.clicked.connect(self.on_click)
-        
+
     def on_click(self):
         print("Button clicked!")
-        self.button.setText("clicked!")
-        
-    
-         
-         
-         
-         
-         
+        self.button.setText("Clicked!")
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
 if __name__ == "__main__":
-    def main():
-            app = QApplication(sys.argv)
-            window = MainWindow()
-            window.show()
-            sys.exit(app.exec_())
+    main()
